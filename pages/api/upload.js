3d9a6986c03879cfdfda88ccad57fe0d15f4dd83
Thanuz Thummala <tthummala@eab.com>
2022-09-16 12:41:05 -0400

    Changes

--- pages/api/upload.js
+++ pages/api/upload.js
@@ -10,7 +10,7 @@ export const config = {
 
 const fileStorageEngine = multer.diskStorage({
     destination: (req, file, cb) => {
-        cb(null, 'public');
+        cb(null, './');
     },
     filename: (req, file, cb) => {
         cb(null, file.originalname);
