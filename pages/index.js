7f9a44db8b1f593938248b05d2f78964f5e35d76
Thanuz Thummala <tthummala@eab.com>
2022-09-16 21:37:20 -0400

    chane

--- pages/index.js
+++ pages/index.js
@@ -7,7 +7,7 @@ export default function Home() {
   let r = useRef();
 
   const [selectedFiles, setSelectedFiles] = useState({});
-  const [setData1, data] = useState();
+  const [setFinalData,finalData] = useState();
   const [showProcessedFiles, setShowProcessedFiles] = useState(false);
   const [isProcessing, setIsProcessing] = useState(false);
   const [isUploading, setIsUploading] = useState(false);
@@ -23,7 +23,7 @@ export default function Home() {
       let f ={data1:d.data.data}
       let ff = {data1:[{name:'thanuz'}]}
       console.log(f);
-      setData1(ff);
+      setFinalData(ff);
     })
   }
   const processFiles = (event) => {
@@ -87,7 +87,7 @@ export default function Home() {
         </form>
 
         <ExcelDownloder
-              data={data}
+              data={finalData}
               filename={'book'}
               type={Type.Button} // or type={'button'}
             >
